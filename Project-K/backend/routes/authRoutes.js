fd5d31abcc501759c5d43e3cade8f68a5943b711
Satya Santosh <145853199+SatyaSantosh197@users.noreply.github.com>
2024-09-19 12:51:28 +0530

    Project-K

--- Project-K/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/register', registerUser);  // Register route
-router.post('/login', loginUser);        // Login route
-
-module.exports = router;

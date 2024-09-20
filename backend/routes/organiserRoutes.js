e5e6c99a7584e74e3f58ba7c0bce83d3e4ba7e48
vihaan-that <vihaanreddy.t22@iiits.in>
2024-09-20 10:15:36 +0530

    Added FetchTeams

--- backend/routes/organiserRoutes.js
+++ backend/routes/organiserRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const organiserController = require('../controllers/organiserController');
+const teamController = require('../controllers/teamControllers');
 
 // Route to create a new organiser
 router.post('/create', organiserController.createOrganiser);
+router.get('/fetchTeams',teamController.fetchTeam);
 
 module.exports = router;

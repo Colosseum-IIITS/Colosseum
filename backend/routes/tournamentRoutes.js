adbe7a50e4790120f46762bfbc2cdcb36ceb6508
vihaan-that <vihaanreddy.t22@iiits.in>
2024-09-26 09:00:50 +0530

    Updated Organiser and tournament Functionality

--- backend/routes/tournamentRoutes.js
+++ backend/routes/tournamentRoutes.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const tournamentController = require('../controllers/tournmentController');
-const { authenticateOrganiser } = require('../middleware/authMiddleware');
+const { authenticateOrganiser  ,authenticateToken, authenticateUser } = require('../middleware/authMiddleware');
 
 // Route to create a new tournament
 router.post('/create', authenticateOrganiser,tournamentController.createTournament);
 router.get('/create',authenticateOrganiser,tournamentController.createTournamentForm);
+router.get('/:tournamentId', authenticateUser, tournamentController.getTournamentById);
 
+router.get('/edit/:tournamentId', authenticateOrganiser, tournamentController.getTournamentEditPage );
+router.post('/join/:tournamentId',authenticateToken,tournamentController.joinTournament);
 // Route to update an existing tournament
 router.post('/update/:tournamentId', tournamentController.updateTournament);
 
 // Route to update the winner by the Organiser of the tournamne
 router.put('/updateWinner', tournamentController.updateWinner);
 
+
+router.post('/edit/:tournamentId', tournamentController.editTournament);
 
 module.exports = router;
     

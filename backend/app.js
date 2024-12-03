ca12927207ac241a88f9c6cfd930db86cea65e8a
vihaan-that <vihaanreddy.t22@iiits.in>
2024-12-03 13:15:38 +0530

    Fixed admin auth

--- backend/app.js
+++ backend/app.js
@@ -53,3 +53,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+console.log('JWT_SECRET_KEY:', process.env.JWT_SECRET_KEY);  // For debugging
